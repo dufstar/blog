1d2eae8b5ac73cd4c1308a963220759a71923e48
ERIC DUFFY <dufstar@ERICs-MacBook-Pro.local>
2019-06-26 18:17:24 -0400

    add about page component

---
+++ imports/ui/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Yebo</h1>
+        <h2>My name's Eric</h2>
+        <div>Wondrous content forthcoming.</div>
+      </div>
+    )
+  }
+}
